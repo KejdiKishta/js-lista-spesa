897a6226adc2fdfeb4fd0ccb0d0a342a48470e67
Kejdi Kishta <155896977+KejdiKishta@users.noreply.github.com>
2024-04-03 17:44:29 +0200

    add: DOM

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
-const shoppingList = ["latte", "biscotti", "pasta", "formaggio"];
+const shoppingList = [];
 
 let listElem = document.querySelector(".lista");
 console.log(listElem);
+let sndBtn = document.querySelector(".aggiungi")
+let newElem = document.querySelector("#new-element")
+console.log(newElem);
+
+sndBtn.addEventListener("click", function (){
+    shoppingList.push(newElem.value);
+    console.log(shoppingList);
+    newElem.value = "";
+
+    let i = 0;
+    let result = "";
+    while (i < shoppingList.length) {
+        result += `<li><input type="checkbox" id="check${i}"><label for="check${i}">${shoppingList[i]}</label></li>`
+        i++;
+    }
+    console.log(result);
+    listElem.innerHTML = result
+})
 
 
-let i = 0;
-let result = "";
-while (i < shoppingList.length) {
-    result += `<li>${shoppingList[i]}</li>`
-    i++;
-}
-console.log(result);
-listElem.innerHTML = result
